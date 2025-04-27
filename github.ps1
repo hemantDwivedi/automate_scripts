dcdf851fe46102392e2c9004b35232839a0e75a8
hemantDwivedi <hemantdwiwedi@gmail.com>
2025-04-28 00:41:30 +0530

    Updated Hello.txt with new content

--- github.ps1
+++ github.ps1
@@ -1,9 +1,22 @@
 $log_time = Get-Date -Format "HH:mm:ss"
 
-$log = "Script started at $log_time`n"
-
-$
+$start_log = "Script started at $log_time`n"
+$end_log = "Script ended at $log_time`n"
 
 Write-Output $log
-Open-GitHubLoginPage -chrome_path $chrome_path -github_login_page $github_login_page
+
+$file = Get-Content -Path "./Hello.txt"
+
+Write-Output "File content: `n"
+Write-Output "$file`n"
+
+Set-Content -Path "./Hello.txt" -Value "Hello, World!"
+
+Start-Sleep 3
+
+git status
+git add .
+git commit -m "Updated Hello.txt with new content"
+git push origin main
+
 Write-Output $log
